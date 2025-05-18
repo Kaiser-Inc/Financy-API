c7b705a3af982fe2cd3cd9efce30b2031fad325f
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-05-17 23:03:35 -0300

    auth:fix(refresh-token): add cord and fix refresh token

--- transactions/src/lib/axios/auth.ts
+++ transactions/src/lib/axios/auth.ts
@@ -6,7 +6,36 @@ export const auth = axios.create({
 	headers: {
 		"Content-Type": "application/json",
 	},
+	withCredentials: true,
 });
+
+auth.interceptors.response.use(
+	(response) => response,
+	async (error) => {
+		const originalRequest = error.config;
+
+		if (error.response?.status === 401 && !originalRequest._retry) {
+			originalRequest._retry = true;
+
+			try {
+				const { data } = await auth.post("/token/refresh", null, {
+					withCredentials: true,
+				});
+
+				// Atualiza headers
+				auth.defaults.headers.common.Authorization = `Bearer ${data.token}`;
+				originalRequest.headers.Authorization = `Bearer ${data.token}`;
+
+				return auth(originalRequest);
+			} catch (refreshError) {
+				console.error("Erro ao tentar refresh token:", refreshError);
+				return Promise.reject(refreshError);
+			}
+		}
+
+		return Promise.reject(error);
+	},
+);
 
 export async function verify(token: string) {
 	const response = await auth.post(
